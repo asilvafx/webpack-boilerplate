1ee17f7f9862af77693d43996c81157cfeac91b5
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-02-06 17:45:17 +0100

    serviceWorker

--- src/App.js
+++ src/App.js
@@ -27,3 +27,10 @@ const webAuthnConfig = {
 };
 const waApp = new WebAuthnApp(webAuthnConfig);
 window.waApp = waApp;
+
+import * as serviceWorker from './serviceWorker.js';
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
